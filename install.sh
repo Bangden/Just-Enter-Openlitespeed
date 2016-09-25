c1e69aee8197f26d921997b3384b830223cc3d29
Bang Den <bangden07@gmail.com>
2016-09-25 09:19:42 +0700

    Installer

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#########################################################
+#														#
+#	OpenLitespeed Just Enter							#
+#	Dibuat oleh : https://www.instagram.com/bangden07	#
+#	Copyright 2016 www.bangden.id - bangden07@gmail.com	#
+#	Didukung oleh : www.indogonetwork.co.id				#
+#														#	
+#########################################################
+yum -y update
+openssl aes-256-cbc -d -a -in konfig.sh -out mulai.sh -pass pass:bangden
+chmod +x mulai.sh
+echo ""
+./mulai.sh
